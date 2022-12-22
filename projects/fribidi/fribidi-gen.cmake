cf826a93f037e8ffe9b554f9654f1c9e16d577f9
wang-bin <wbsecg1@gmail.com>
2022-12-22 22:57:09 +0800

    add cmake project for libass

---
+++ projects/fribidi/fribidi-gen.cmake
@@ -0,0 +1,56 @@
+set(SRC_GEN_DIR ${SRC_DIR}/gen.tab)
+add_executable(gen-unicode-version ${SRC_GEN_DIR}/gen-unicode-version.c)
+add_executable(gen-bidi-type-tab ${SRC_GEN_DIR}/gen-bidi-type-tab.c ${SRC_GEN_DIR}/packtab.c)
+add_executable(gen-brackets-tab ${SRC_GEN_DIR}/gen-brackets-tab.c ${SRC_GEN_DIR}/packtab.c)
+add_executable(gen-brackets-type-tab ${SRC_GEN_DIR}/gen-brackets-type-tab.c ${SRC_GEN_DIR}/packtab.c)
+add_executable(gen-joining-type-tab ${SRC_GEN_DIR}/gen-joining-type-tab.c ${SRC_GEN_DIR}/packtab.c)
+add_executable(gen-arabic-shaping-tab ${SRC_GEN_DIR}/gen-arabic-shaping-tab.c)
+add_executable(gen-mirroring-tab ${SRC_GEN_DIR}/gen-mirroring-tab.c ${SRC_GEN_DIR}/packtab.c)
+
+add_dependencies(gen-bidi-type-tab gen-unicode-version)
+add_dependencies(gen-brackets-tab gen-unicode-version)
+add_dependencies(gen-brackets-type-tab gen-unicode-version)
+add_dependencies(gen-joining-type-tab gen-unicode-version)
+add_dependencies(gen-arabic-shaping-tab gen-unicode-version)
+add_dependencies(gen-mirroring-tab gen-unicode-version)
+
+# TODO: use OUTPUT instead of TARGET
+add_custom_command(TARGET gen-unicode-version POST_BUILD
+  COMMAND gen-unicode-version unidata/ReadMe.txt unidata/BidiMirroring.txt gen-unicode-version.c >fribidi-unicode-version.h
+  WORKING_DIRECTORY "${SRC_GEN_DIR}"
+)
+add_custom_command(TARGET gen-bidi-type-tab
+  #COMMAND gen-bidi-type-tab unidata/DerivedBidiClass.txt fribidi-unicode-version.h gen-bidi-type-tab.c packtab.c packtab.h > derived_bidi-type.tab.i
+  COMMAND gen-bidi-type-tab 2 unidata/UnicodeData.txt > bidi-type.tab.i
+  WORKING_DIRECTORY "${SRC_GEN_DIR}"
+)
+add_custom_command(TARGET gen-joining-type-tab
+  COMMAND gen-joining-type-tab 2 unidata/UnicodeData.txt unidata/ArabicShaping.txt >joining-type.tab.i
+  WORKING_DIRECTORY "${SRC_GEN_DIR}"
+)
+add_custom_command(TARGET gen-arabic-shaping-tab
+  COMMAND gen-arabic-shaping-tab 2 unidata/UnicodeData.txt >arabic-shaping.tab.i
+  WORKING_DIRECTORY "${SRC_GEN_DIR}"
+)
+add_custom_command(TARGET gen-mirroring-tab
+  COMMAND gen-mirroring-tab 2 unidata/BidiMirroring.txt >mirroring.tab.i
+  WORKING_DIRECTORY "${SRC_GEN_DIR}"
+)
+add_custom_command(TARGET gen-brackets-tab
+  COMMAND gen-brackets-tab 2 unidata/BidiBrackets.txt unidata/UnicodeData.txt > brackets.tab.i
+  WORKING_DIRECTORY "${SRC_GEN_DIR}"
+)
+add_custom_command(TARGET gen-brackets-type-tab
+  COMMAND gen-brackets-type-tab 2 unidata/BidiBrackets.txt > brackets-type.tab.i
+  WORKING_DIRECTORY "${SRC_GEN_DIR}"
+)
+
+add_custom_target(gen_tab
+  DEPENDS gen-unicode-version gen-bidi-type-tab gen-joining-type-tab gen-arabic-shaping-tab gen-mirroring-tab
+)
+
+include_directories(
+    ${SRC_DIR}/lib
+    ${SRC_GEN_DIR}
+    ${PROJECT_BINARY_DIR}
+    )
---
+++ projects/fribidi/fribidi-lib.cmake
@@ -0,0 +1,45 @@
+aux_source_directory(${SRC_DIR}/lib FRIBIDI_SRC)
+list(APPEND FRIBIDI_SRC
+    ${SRC_DIR}/lib/arabic-misc.tab.i
+)
+
+file(READ ${SRC_DIR}/lib/fribidi.def DEF_CONTENT)
+file(WRITE "${PROJECT_BINARY_DIR}/${PROJECT_NAME}.def" "EXPORTS\n${DEF_CONTENT}")
+add_library(fribidi_static STATIC ${FRIBIDI_SRC})
+target_compile_definitions(fribidi_static PRIVATE FRIBIDI_LIB_STATIC)
+set(FRIBIDI_TARGETS fribidi_static)
+# TODO:
+file(GLOB HEADERS "fribidi*.h" "${PROJECT_SOURCE_DIR}/gen.tab/fribidi-*.h" "${PROJECT_BINARY_DIR}/fribidi-*.h")
+
+if(FRIBIDI_SHARED)
+    add_library(fribidi_shared SHARED ${FRIBIDI_SRC} ${PROJECT_BINARY_DIR}/${PROJECT_NAME}.def)
+    set_target_properties(fribidi_shared PROPERTIES
+        VERSION ${PROJECT_VERSION}
+        SOVERSION ${SO_VERSION}
+        OUTPUT_NAME "fribidi"
+        CLEAN_DIRECT_OUTPUT 1
+        PUBLIC_HEADER "${HEADERS}"
+        INTERPROCEDURAL_OPTIMIZATION 1 # TODO: option
+        #LINK_DEF_FILE_FLAG ${SRC_DIR}/lib/fribidi.def # TODO:
+        )
+    list(APPEND FRIBIDI_TARGETS fribidi_shared)
+endif()
+
+#static lib use name libxxx.lib, shared lib use xxx.lib, like ucrt does
+if(MSVC)
+  set_target_properties(fribidi_static PROPERTIES OUTPUT_NAME "lib${PROJECT_NAME}")
+else()
+  set_target_properties(fribidi_static PROPERTIES OUTPUT_NAME "${PROJECT_NAME}")
+endif()
+set_target_properties(fribidi_static PROPERTIES CLEAN_DIRECT_OUTPUT 1)
+
+if(WIN32)
+  install(FILES ${PROJECT_BINARY_DIR}/${PROJECT_NAME}.def DESTINATION lib)
+endif()
+install(TARGETS ${FRIBIDI_TARGETS}
+  EXPORT ${PROJECT_NAME}
+  RUNTIME DESTINATION bin # *.dll COMPONENT bin
+  LIBRARY DESTINATION lib COMPONENT shlib
+  ARCHIVE DESTINATION lib COMPONENT dev
+  PUBLIC_HEADER DESTINATION include/${PROJECT_NAME} COMPONENT dev
+)

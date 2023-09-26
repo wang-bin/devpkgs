6e642349ca47091b6bb4732a3b402d2f5cecf66f
wang-bin <wbsecg1@gmail.com>
2023-09-26 22:50:23 +0800

    dav1d: support simd

--- mkpkg.sh
+++ mkpkg.sh
@@ -9,11 +9,11 @@ mkdir -p dep/lib/WinRT/{arm64,x64,x86}
 
 7z x -y devpkgs-windows-desktop-Release-vs2022.7z
 cp -avf install/include/va dep/include/
-cp -avf install/x64/include/{ass,GLFW,mfx,vpl,*.h} dep/include
+cp -avf install/x64/include/{ass,dav1d,GLFW,mfx,vpl,*.h} dep/include
 for A in arm64 x64 x86; do
-    mv install/$A/bin/{libass,zlib}.dll dep/bin/windows/$A/
+    mv install/$A/bin/{libass,*dav1d,zlib}.dll dep/bin/windows/$A/
     mv install/$A/share/pkgconfig/zlib.pc dep/lib/windows/$A/pkgconfig            # TODO: edit zlib.pc
-    mv install/$A/lib/{glfw3,snappy,va*,zlib*}.lib dep/lib/windows/$A/MD/
+    mv install/$A/lib/{dav1d,glfw3,snappy,va*,zlib*}.lib dep/lib/windows/$A/MD/
     if [ -f install/$A/lib/vpl.lib ]; then
         mv install/$A/lib/{mfx,vpl}.lib dep/lib/windows/$A/MD/
         mv install/$A/lib/pkgconfig/{libmfx,vpl}.pc dep/lib/windows/$A/pkgconfig/     # TODO: edit libmfx.pc
@@ -30,15 +30,15 @@ rm -rf install
 
 7z x -y devpkgs-windows-desktop-Release-vs2022-ltl.7z
 for A in x64 x86; do
-    mv install/$A/bin/{libass,zlib}.dll dep/bin/windows/$A/LTL
-    mv install/$A/lib/{glfw3,mfx,snappy,vpl,zlib*}.lib dep/lib/windows/$A/MT/
+    mv install/$A/bin/{libass,*dav1d,zlib}.dll dep/bin/windows/$A/LTL
+    mv install/$A/lib/{dav1d,glfw3,mfx,snappy,vpl,zlib*}.lib dep/lib/windows/$A/MT/
 done
 rm -rf install
 
 7z x -y devpkgs-uwp-Release-vs2022.7z
 for A in x64 arm64; do
-    mv install/$A/bin/{libass,zlib}.dll dep/bin/WinRT/$A
-    mv install/$A/lib/{snappy,zlib*}.lib dep/lib/WinRT/$A
+    mv install/$A/bin/{libass,*dav1d,zlib}.dll dep/bin/WinRT/$A
+    mv install/$A/lib/{dav1d,snappy,zlib*}.lib dep/lib/WinRT/$A
 done
 rm -rf install
 

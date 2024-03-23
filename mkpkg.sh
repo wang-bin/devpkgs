346988a9e0fb2e7e2556e05aa4be2796a38ad201
wang-bin <wbsecg1@gmail.com>
2024-03-23 09:08:48 +0800

    visionOS

--- mkpkg.sh
+++ mkpkg.sh
@@ -79,4 +79,9 @@ tar xvf devpkgs-tvOS-MinSizeRel.tar.xz
 mv install/arm64/lib/*.framework dep/lib/tvOS/
 rm -rf install
 
+mkdir -p dep/lib/visionOS
+tar xvf devpkgs-visionOS-MinSizeRel.tar.xz
+mv install/arm64/lib/*.framework dep/lib/visionOS/
+rm -rf install
+
 7z a -ssc -m0=lzma2 -mx=9 -ms=on -mf=off dep.7z dep

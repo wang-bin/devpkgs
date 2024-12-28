4fa4d0b29b4d40db8cd34666b814aaafaa9d47f5
wang-bin <wbsecg1@gmail.com>
2024-12-28 11:04:56 +0800

    install libass.lib

--- mkpkg.sh
+++ mkpkg.sh
@@ -12,7 +12,7 @@ sed_bak=
 uname |grep -iq darwin && sed_bak=".bak"
 
 7z x -y devpkgs-windows-desktop-Release-vs2022.7z
-rsync -avm --include='*/' --include='*freetype*' --include='*harfbuzz*' --include='**/freetype2/**' --include='**/harfbuzz/**' --exclude='*' install/* dep-av/windows
+rsync -avm --include='*/' --include='*freetype*' --include='*fribidi*' --include='*harfbuzz*' --include='*ass.*' --include='**/ass/**' --include='**/freetype2/**' --include='**/fribidi/**' --include='**/harfbuzz/**' --exclude='*' install/* dep-av/windows
 find dep-av/windows -name "*.pc" -exec sed -i $sed_bak '/-lm/d' {} \;   # harfbuzz.pc -lm
 cp -avf install/include/va dep/include/
 cp -avf install/x64/include/{ass,dav1d,GLFW,mfx,vpl,*.h} dep/include
@@ -35,7 +35,8 @@ done
 rm -rf install
 
 7z x -y devpkgs-windows-desktop-Release-vs2022-ltl.7z
-rsync -avm --include='*/' --include='*freetype*' --include='*harfbuzz*' --include='**/freetype2/**' --include='**/harfbuzz/**' --exclude='*' install/* dep-av/windows-ltl
+rsync -avm --include='*/' --include='*freetype*' --include='*fribidi*' --include='*harfbuzz*' --include='*ass.*' --include='**/ass/**' --include='**/freetype2/**' --include='**/fribidi/**' --include='**/harfbuzz/**' --exclude='*' install/* dep-av/windows-ltl
+find dep-av/windows-ltl -name "*.pc" -exec sed -i $sed_bak '/-lm/d' {} \;   # harfbuzz.pc -lm
 for A in x64 x86; do
     mv install/$A/bin/{libass,*dav1d,zlib}.dll dep/bin/windows/$A/LTL
     mv install/$A/lib/{dav1d,glfw3,mfx,snappy,vpl,zlib*}.lib dep/lib/windows/$A/MT/
@@ -43,7 +44,8 @@ done
 rm -rf install
 
 7z x -y devpkgs-uwp-Release-vs2022.7z
-rsync -avm --include='*/' --include='*freetype*' --include='*harfbuzz*' --include='**/freetype2/**' --include='**/harfbuzz/**' --exclude='*' install/* dep-av/uwp
+rsync -avm --include='*/' --include='*freetype*' --include='*fribidi*' --include='*harfbuzz*' --include='*ass.*' --include='**/ass/**' --include='**/freetype2/**' --include='**/fribidi/**' --include='**/harfbuzz/**' --exclude='*' install/* dep-av/uwp
+find dep-av/uwp -name "*.pc" -exec sed -i $sed_bak '/-lm/d' {} \;   # harfbuzz.pc -lm
 for A in x64 x86 arm64; do
     mv install/$A/bin/{libass,*dav1d,zlib}.dll dep/bin/WinRT/$A
     mv install/$A/lib/{dav1d,snappy,zlib*}.lib dep/lib/WinRT/$A
@@ -72,7 +74,7 @@ rm -rf install
 mkdir -p dep/lib/android/{arm64-v8a,armeabi-v7a}
 7z x -y devpkgs-android-MinSizeRel.7z
 rsync -avm --include='*/' --include='*wolfssl*' --include='**/wolfssl/**' --exclude='*' install/* dep-av/android
-rsync -avm --include='*/' --include='*freetype*' --include='*harfbuzz*' --include='**/freetype2/**' --include='**/harfbuzz/**' --exclude='*' install/* dep-av/android
+rsync -avm --include='*/' --include='*freetype*' --include='*fribidi*' --include='*harfbuzz*' --include='*ass.*' --include='**/ass/**' --include='**/freetype2/**' --include='**/fribidi/**' --include='**/harfbuzz/**' --exclude='*' install/* dep-av/android
 for A in arm64-v8a armeabi-v7a; do
     mv install/$A/lib/*.so dep/lib/android/$A/
 done
@@ -81,7 +83,7 @@ rm -rf install
 
 mkdir -p dep/lib/macOS
 tar xvf devpkgs-macOS-MinSizeRel.tar.xz
-rsync -avm --include='*/' --include='*freetype*' --include='*harfbuzz*' --include='**/freetype2/**' --include='**/harfbuzz/**' --exclude='*' install/* dep-av/macOS
+rsync -avm --include='*/' --include='*freetype*' --include='*fribidi*' --include='*harfbuzz*' --include='*ass.*' --include='**/ass/**' --include='**/freetype2/**' --include='**/fribidi/**' --include='**/harfbuzz/**' --exclude='*' install/* dep-av/macOS
 mv dep-av/macOS/arm64/include dep-av/macOS/
 mv dep-av/macOS/arm64/lib/pkgconfig dep-av/macOS/lib
 rm -rf dep-av/macOS/{arm,x86_}64
@@ -92,21 +94,21 @@ rm -rf install
 mkdir -p dep/lib/iOS
 tar xvf devpkgs-iOS-MinSizeRel.tar.xz
 rsync -avm --include='*/' --include='*wolfssl*' --include='**/wolfssl/**' --exclude='*' install/arm64/* dep-av/iOS
-rsync -avm --include='*/' --include='*freetype*' --include='*harfbuzz*' --include='**/freetype2/**' --include='**/harfbuzz/**' --exclude='*' install/arm64/* dep-av/iOS
+rsync -avm --include='*/' --include='*freetype*' --include='*fribidi*' --include='*harfbuzz*' --include='*ass.*' --include='**/ass/**' --include='**/freetype2/**' --include='**/fribidi/**' --include='**/harfbuzz/**' --exclude='*' install/* dep-av/iOS
 mv install/arm64/lib/*.framework dep/lib/iOS/
 rm -rf install
 
 mkdir -p dep/lib/tvOS
 tar xvf devpkgs-tvOS-MinSizeRel.tar.xz
 rsync -avm --include='*/' --include='*wolfssl*' --include='**/wolfssl/**' --exclude='*' install/arm64/* dep-av/tvOS
-rsync -avm --include='*/' --include='*freetype*' --include='*harfbuzz*' --include='**/freetype2/**' --include='**/harfbuzz/**' --exclude='*' install/arm64/* dep-av/tvOS
+rsync -avm --include='*/' --include='*freetype*' --include='*fribidi*' --include='*harfbuzz*' --include='*ass.*' --include='**/ass/**' --include='**/freetype2/**' --include='**/fribidi/**' --include='**/harfbuzz/**' --exclude='*' install/* dep-av/tvOS
 mv install/arm64/lib/*.framework dep/lib/tvOS/
 rm -rf install
 
 mkdir -p dep/lib/visionOS
 tar xvf devpkgs-visionOS-MinSizeRel.tar.xz
 rsync -avm --include='*/' --include='*wolfssl*' --include='**/wolfssl/**' --exclude='*' install/arm64/* dep-av/visionOS
-rsync -avm --include='*/' --include='*freetype*' --include='*harfbuzz*' --include='**/freetype2/**' --include='**/harfbuzz/**' --exclude='*' install/arm64/* dep-av/visionOS
+rsync -avm --include='*/' --include='*freetype*' --include='*fribidi*' --include='*harfbuzz*' --include='*ass.*' --include='**/ass/**' --include='**/freetype2/**' --include='**/fribidi/**' --include='**/harfbuzz/**' --exclude='*' install/* dep-av/visionOS
 mv install/arm64/lib/*.framework dep/lib/visionOS/
 rm -rf install
 
